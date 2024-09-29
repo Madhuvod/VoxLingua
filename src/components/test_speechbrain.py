f8dc01d899962a1d3696f8d4aca40c6ac710ab82
Madhu <madhushantangot@gmail.com>
2024-09-29 18:39:28 +0530

    deletions of unnecessary files in comp

--- src/components/test_speechbrain.py
+++
@@ -1,8 +0,0 @@
-from speechbrain.inference import EncoderClassifier
-
-print("SpeechBrain imported successfully!")
-
-# Try to load a pre-trained model
-spk_model = EncoderClassifier.from_hparams(source="speechbrain/spkrec-ecapa-voxceleb")
-
-print("Pre-trained model loaded successfully!")
